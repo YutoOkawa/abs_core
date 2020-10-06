a1d9259df0e2974def34d1f8e11d1debf3b24f23
大川悠人 <ookawayuuto@ookawayuuninnoMacBook-Pro.local>
2020-10-06 12:32:09 +0900

    tpkSetupの実装

---
+++ abs.js
@@ -0,0 +1,70 @@
+const CTX = require("amcl-js").CTX;
+
+var ctx = new CTX("BN254");
+
+var ABS_OK = 0;
+var ABS_FAIL = -1;
+
+function generateG1Element(rng) {
+    var seed = ctx.FP.rand(rng);
+    var G = ctx.ECP.map2point(seed);
+    return G;
+}
+
+function generateG2Element(rng) {
+    var seed = ctx.FP2.rand(rng);
+    var H = ctx.ECP2.map2point(seed);
+    return H;
+}
+
+function setOpt(obj, prop, val) {
+    if (val !== undefined) {
+        obj[prop] = val;
+    } else {
+        console.log("undefined")
+    }
+}
+
+exports.init = function(ctx) {
+    var G = ctx.ECP.generator();
+    if (G.is_infinity()) {
+        return ABS_FAIL
+    }
+    
+    var H = ctx.ECP2.generator();
+    if (H.is_infinity()) {
+        return ABS_FAIL
+    }
+
+    return ABS_OK
+};
+
+exports.trusteesetup = function(ctx, attributes, rng) {
+    var tpk = {};
+    var tmax = attributes.length;
+    var G = generateG1Element(rng);
+    setOpt(tpk, "g", G);
+
+    for (var i=0; i<tmax; i++) {
+        var h = generateG2Element(rng);
+        setOpt(tpk, "h"+String(i), h);
+    }
+
+    return tpk;
+};
+
+exports.authoritysetup = function(ctx) {
+
+};
+
+exports.generateattributes = function(ctx){
+    
+};
+
+exports.sign = function(ctx) {
+
+};
+
+exports.verify = function(ctx) {
+
+};
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const CTX = require("amcl-js").CTX;
 
 var ctx = new CTX("BN254");
 
+const abs = require("./abs");
+
 var outputBytes = function(G) {
     var W = [];
     G.toBytes(W, true);
@@ -9,7 +11,13 @@ var outputBytes = function(G) {
 }
 
 var i;
+// 乱数の初期化
+var RAW = [];
 var rng = new ctx.RAND();
+rng.clean();
+for (i=0;i<100;i++) RAW[i]=i;
+rng.seed(100,RAW);
+
 var r = new ctx.BIG(0);
 r.rcopy(ctx.ROM_CURVE.CURVE_Order);
 
@@ -20,7 +28,7 @@ var GH = ctx.PAIR.initmp();
 GH = ctx.PAIR.ate(H, G);
 var v = ctx.PAIR.fexp(GH);
 
-var sGH = ctx.PAIR.initmp();
+var sGH = ctx.PAIR.initmp()
 var s = ctx.BIG.randtrunc(r, 2 * ctx.CURVE_Order, rng);
 var sG = ctx.PAIR.G1mul(G, s);
 sGH = ctx.PAIR.ate(H, sG);
@@ -29,3 +37,6 @@ outputBytes(sgh);
 
 var powGH = ctx.PAIR.GTpow(v, s);
 outputBytes(powGH);
+
+var tpk = abs.trusteesetup(ctx, ["Aqours", "Guilty Kiss", "AZALEA", "CYaRon"], rng);
+console.log(tpk);
